259a87ce1fd09539b2cb8e090e9aed65e4e36804
Jean-Philippe Ouellet <jpo@vt.edu>
2014-10-01 18:20:12 -0400

    initial commit (2.26 upstream)

---
+++ version.h
@@ -0,0 +1,9 @@
+/* version.h - version defines for thttpd and libhttpd */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define SERVER_SOFTWARE "thttpd/2.26 14aug2014"
+#define SERVER_ADDRESS "http://www.acme.com/software/thttpd/"
+
+#endif /* _VERSION_H_ */
